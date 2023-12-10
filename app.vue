5dbf357ea35f8c88d8e8e326b5418611feb5bd60
annead0 <120595101+annead0@users.noreply.github.com>
2023-12-10 17:12:26 +0000

    update

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
     <UCard class="mt-10">
         <div class="flex justify-between">
           <h1 class="text-xl font-large"><b>Fotohaven</b></h1>
+          <NuxtLink to="/"></NuxtLink>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
     </UCard>
