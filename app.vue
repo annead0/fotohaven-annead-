12690f6f901329dc7d2da1914de2902eb8cb48bc
annead0 <120595101+annead0@users.noreply.github.com>
2023-12-10 16:35:49 +0000

    update

--- app.vue
+++ app.vue
@@ -1,11 +1,7 @@
-<script>
-const details={
-  title:"Fotohaven"
-}
-</script>
+
 <template>
 <UContainer>
-    <h1 style="text-decoration:underline; text-align:center;"><b>{{details.title}}</b></h1>
+    <h1 style="text-decoration:underline; text-align:center;"><b>Fotohaven</b></h1>
     <UCard class="mt-10">
         <div class="flex justify-between">
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
