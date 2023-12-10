340c0c74e00b66074bd251a71855badef65adc75
annead0 <120595101+annead0@users.noreply.github.com>
2023-12-10 16:22:17 +0000

    Add title

--- app.vue
+++ app.vue
@@ -1,13 +1,22 @@
+<script>
+const tClass =ref("headingtitle");
+</script>
 <template>
-  <UContainer>
+<UContainer>
+  <template #header>
+    <h1 :class="tClass">Fotohaven</h1>
     <UCard class="mt-10">
-      <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
     </UCard>
+    </template>
   </UContainer>
 </template>
+<style>
+#headingtitle{
+  text-decoration:underline;
+  text-align:center;
+  font-weight:bold;
+}
+</style>
