b8e0916e88c6b455ebf8d364cd0b8b86f7cfaaf1
annead0 <120595101+annead0@users.noreply.github.com>
2023-12-10 16:30:43 +0000

    update

--- app.vue
+++ app.vue
@@ -5,14 +5,12 @@ const details={
 </script>
 <template>
 <UContainer>
-  <template #header>
     <h1 style="text-decoration:underline; text-align:center;"><b>{{details.title}}</b></h1>
     <UCard class="mt-10">
         <div class="flex justify-between">
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
     </UCard>
-    </template>
   </UContainer>
 </template>
 <style>
